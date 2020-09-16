d1669d02dd464f306400692049ada7f96a6a3aa1
degr8sid-code <degr8sid-code@users.noreply.github.com>
2020-09-16 23:08:13 +0500

    Add project files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+//section 1
 import { Component } from '@angular/core';
 
+//section 2: component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+//section 3: component logic
 export class AppComponent {
   title = 'my-app';
 }
